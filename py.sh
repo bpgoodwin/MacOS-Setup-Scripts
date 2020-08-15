a4071a938f36224d95913cac9244c54c3cd33134
bpgoodwin <bpgoodwin@protonmail.ch>
2020-08-15 12:19:40 +0800

    global changes

--- py.sh
+++ py.sh
@@ -17,37 +17,14 @@ easy_install pip
 ###############################################################################
 
 echo "------------------------------"
-echo "Setting up virtual environments."
+echo "Setting up Global Python."
 
 # Install virtual environments globally
 # It fails to install virtualenv if PIP_REQUIRE_VIRTUALENV was true
+
 export PIP_REQUIRE_VIRTUALENV=false
 pip install virtualenv
 pip install virtualenvwrapper
-
-echo "------------------------------"
-echo "Source virtualenvwrapper from ~/.extra"
-
-EXTRA_PATH=~/.extra
-echo $EXTRA_PATH
-echo "" >> $EXTRA_PATH
-echo "" >> $EXTRA_PATH
-echo "# Source virtualenvwrapper, added by pydata.sh" >> $EXTRA_PATH
-echo "export WORKON_HOME=~/.virtualenvs" >> $EXTRA_PATH
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> $EXTRA_PATH
-echo "" >> $BASH_PROFILE_PATH
-source $EXTRA_PATH
-
-###############################################################################
-# Python 3 Virtual Enviroment                                                 #
-###############################################################################
-
-echo "------------------------------"
-echo "Setting up py3-data virtual environment."
-
-# Create a Python3 data environment
-mkvirtualenv --python=/usr/local/bin/python3 py3-data
-workon py3-data
 
 # Install Python data modules
 pip install numpy
@@ -65,17 +42,5 @@ pip install Flask
 pip install sqlalchemy
 pip install mysqlclient
 
-###############################################################################
-# Install IPython Profile
-###############################################################################
-
-echo "------------------------------"
-echo "Installing IPython Notebook Default Profile"
-
-# Add the IPython profile
-mkdir -p ~/.ipython
-cp -r init/profile_default/ ~/.ipython/profile_default
-
 echo "------------------------------"
 echo "Script completed."
-echo "Usage: workon py3-data for Python3"
